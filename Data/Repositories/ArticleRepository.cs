df9ab1014c14ba6b59b2bace4f99332b0c941d91
nelson9 <nferguson@macmillan.org.uk>
2016-09-15 16:56:10 +0100

    added db context

--- Data/Repositories/ArticleRepository.cs
+++ Data/Repositories/ArticleRepository.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.GenericRepo;
+using NewsApp.Models;
 
 namespace Data.Repositories
 {
-    class ArticleRepository
+    public interface IArticleRepository : IRepository<Article>
     {
+        
+    }
+    public class ArticleRepository : Repository<Article>, IArticleRepository
+    {
+        public NewsContext NewsContext
+        {
+            get
+            {
+                return Context as NewsContext;
+            }
+        }
+
+        public ArticleRepository(NewsContext context) : base(context) { }
     }
 }
--- NewsApp/Controllers/HomeController.cs
+++ NewsApp/Controllers/HomeController.cs
@@ -1,18 +1,55 @@
-﻿using System;
+﻿using Data.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewsApp.Models;
 
 namespace NewsApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IArticleRepository _articleRepository;
+
+        public HomeController(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            try
+            {
+                _articleRepository.Add(new Article
+                {
+                    Title = "tiele",
+                    Body = "body",
+                    Likes = 1,
+                    PublishDate = DateTime.Now,
+                    Comments = new List<Comment>
+                    {
+                        new Comment
+                        {
+                            Text = "hello",
+                        },
+                        new Comment
+                        {
+                            Text = "sdf"
+                        }
+                    }
+                });
+                _articleRepository.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception(e.Message);
+            }
+            var articles = _articleRepository.GetAll();
 
-            return View();
+            return View(articles);
         }
     }
 }
